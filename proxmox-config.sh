519b8b390eae93e4b7ba7063b895197b77974aae
Oslander <contact@samuelmcpherson.net>
2021-09-14 17:43:20 -0700

    Added passthrough support for AMD CPUs and required option to specify which CPU type
    Added check for zsh user shell, added comments throghout script

--- proxmox-config.sh
+++ proxmox-config.sh
@@ -4,6 +4,8 @@ USER=$1
 
 USERPASS=$2
 
+USERSHELL=/bin/zsh
+
 ZFS=yes
 # Setting this variable will create separate ZFS datasets for user directories, backups, VM image files, and templates saved on the local ZFS storage. It also installs Sanoid to manage ZFS snapshots systemwide. 
 
@@ -22,6 +24,10 @@ ANSIBLE=yes
 PASSTHROUGH=yes
 # Setting this variable will enable the kernel modules and boot options required for PCI passthrough on Intel based systems; to use this option, one of the following two variables will need to be set in order to correctly modify the kernel commandline of the correct bootloader
 
+INTEL_CPU=yes
+
+AMD_CPU=
+
 EFI=
 # This variable determines what bootloader configuration is modified when setting up PCI passthrough, EFI installations use systemdboot
 
@@ -29,7 +35,7 @@ BIOS=yes
 # This variable determines what bootloader configuration is modified when setting up PCI passthrough, BIOS installations use grub
 
 export DEBIAN_FRONTEND=noninteractive
-# make sure that interactive prompts do not interrupt any system upgrades or package installations
+# Make sure that interactive prompts do not interrupt any system upgrades or package installations
 
 if [[ -n "$COMMUNITYREPO" ]]
 then 
@@ -44,85 +50,118 @@ apt update && apt -y upgrade
 # Fully upgrades the system 
 
 apt install -y sudo rsync dosfstools zsh curl patch wget git irssi lynx elinks htop lm-sensors net-tools screen tmux sysstat iotop ripgrep nmap iftop vim neovim tcpdump smartmontools
+# Install a set of base packages 
 
 if [[ -n "$ZFS" ]]
 then
-    DEBIAN_FRONTEND=noninteractive apt install -y debhelper libcapture-tiny-perl libconfig-inifiles-perl pv lzop mbuffer
+    apt install -y debhelper libcapture-tiny-perl libconfig-inifiles-perl pv lzop mbuffer
+    # Install requirements to build and install sanoid for ZFS snapshot management
 
     rm -r /var/lib/vz/dump
     rm -r /var/lib/vz/images
     rm -r /var/lib/vz/template
+    # Remove default image storage directories to replace with dedicated ZFS datasets 
+
 
     zfs create -o canmount=off -o xattr=sa -o compression=lz4 -o recordsize=16k rpool/data/VM_image_data
-    zfs create -o mountpoint=/var/lib/vz/dumln -s ~/.local/share/kwin/scripts/krohnkite/metadata.desktop ~/.local/share/kservices5/krohnkite.desktopp rpool/data/VM_image_data/dump
+    # Create VM_image_data parent dataset with properties to optimize VM performance on any child datasets
+
+    zfs create -o mountpoint=/var/lib/vz/dump rpool/data/VM_image_data/dump
     zfs create -o mountpoint=/var/lib/vz/images rpool/data/VM_image_data/images
     zfs create -o mountpoint=/var/lib/vz/template rpool/data/VM_image_data/template
-    zfs create -o canmount=off rpool/data/users
+    # Create child datasets for image storage
     
-    cd /root && git clone https://github.com/jimsalterjrs/sanoid.git
+    zfs create -o canmount=off rpool/data/users
+    # Create separate dataset for user accounts
+    
+    cd /tmp && git clone https://github.com/jimsalterjrs/sanoid.git
+    # Pull the github repo to install sanoid
 
-    cd /root/sanoid && git checkout $(git tag | grep '^v' | tail -n 1) && ln -s packages/debian . && dpkg-buildpackage -uc -us
+    cd /tmp/sanoid && git checkout $(git tag | grep '^v' | tail -n 1) && ln -s packages/debian . && dpkg-buildpackage -uc -us
+    # Checkout the latest stable version of sanoid and build the debian package
 
-    apt install -y /root/sanoid_*_all.deb
+    apt install -y /tmp/sanoid_*_all.deb
+    # Install the built sanoid package
+
+    systemctl enable sanoid.timer
+    # Enable the sanoid shedule
 
     zfs create -o mountpoint=/home/$USER rpool/data/users/$USER
-    
-    useradd -M -g users -G sudo,adm,plugdev -s /usr/bin/zsh -d /home/$USER $USER
+    # Create main user home directory dataset
+
+    useradd -M -g users -G sudo,adm,plugdev -s $USERSHELL -d /home/$USER $USER
+    # Create main user for non-root configuration
 
     if [[ -n "$ANSIBLE" ]]
     then
         zfs create -o mountpoint=/home/ansible rpool/data/users/ansible
-    
+        # Create home directory for separate ansible user
+
         useradd -M -s /bin/bash -d /home/ansible ansible
+        # Create separate ansible user
     fi
 
 elif [[ -z "$ZFS" ]]
 then
-    useradd -m -g users -G sudo,adm,plugdev -s /usr/bin/zsh $USER
+    useradd -m -g users -G sudo,adm,plugdev -s $USERSHELL $USER
+    # Create main user for non-root configuration
 
     if [[ -n "$ANSIBLE" ]]
     then
         useradd -m -s /bin/bash ansible
+        # Create separate ansible user
     fi
 fi
 
 mkdir -p /home/$USER/.ssh
 
 cd /home/$USER && git clone $CONFIGREPO
+# Pull git repository with configuration files to copy to new install
 
-cp $CONFIGDIR/home/.zshrc /home/$USER/.zshrc
+if [[ "$USERSHELL" = "/bin/zsh" || "$USERSHELL" = "/usr/bin/zsh" ]]
+then
+    cp $CONFIGDIR/home/.zshrc /home/$USER/.zshrc
+    
+    cp $CONFIGDIR/home/.zshrc.local /home/$USER/.zshrc.local
 
-cp $CONFIGDIR/home/.zshrc.local /home/$USER/.zshrc.local
-
-cp $CONFIGDIR/home/grml-zsh-refcard.pdf /home/$USER/grml-zsh-refcard.pdf
+    cp $CONFIGDIR/home/grml-zsh-refcard.pdf /home/$USER/grml-zsh-refcard.pdf
+fi
+# Copy grml zsh configuration to main user home directory is zsh is used as the shell
 
 if [[ -n "$ANSIBLE" ]]
 then
     mkdir -p /home/ansible/.ssh
 
     cp $CONFIGDIR/home/ansible/authorized_keys /home/ansible/.ssh/authorized_keys
+    # Copy a pre-configured public key fingerprint from the config files repository to the ~/.ssh/authorized_keys file for the ansible user to allow key based authentication to this user
 
     chown -R ansible:ansible /home/ansible
 
     echo 'ansible ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/030_ansible-nopasswd
+    # Allow ansible user to use sudo without a password
 
 fi
 
 cp $CONFIGDIR/etc/ssh/ssh_config /etc/ssh/ssh_config
+# Copy ssh_config file to the new system, current version is used to avoid issues when connecting the older Cisco switches
 
 cp $CONFIGDIR/etc/ssh/sshd_config /etc/ssh/sshd_config
+# Copy sshd_config file to the the new system, current version restricts ssh access to key based only
 
 if [[ -n "$ZFS" ]]
 then
     mkdir /etc/sanoid
     cp $CONFIGDIR/etc/sanoid/proxmox/sanoid.conf /etc/sanoid/sanoid.conf
+    # Copy sanoid configuration file into place on the new system
 fi
 
 cp $CONFIGDIR/home/.gitconfig /home/$USER/.gitconfig
+# Copy a configured git configuration file to the main user account
 
 chown -R $USER:users /home/$USER
 
 echo "$USER:$USERPASS" | chpasswd
+# Set the main user password 
 
 if [[ -n "$PASSTHROUGH" ]]
 then 
@@ -130,19 +169,39 @@ then
     echo 'vfio_iommu_type1' >> /etc/modules
     echo 'vfio_pci' >> /etc/modules
     echo 'vfio_virqfd' >> /etc/modules
-    
+    # Adds kernel modules required for pci passthrough
+
     if [[ -n "$EFI" ]]
     then
-        sed -i 's/root=.*/& intel_iommu=on iommu=pt/g' /etc/kernel/cmdline
+        if [ -n "$INTEL_CPU" ] && [ -z "$AMD_CPU" ]
+        then
+            sed -i 's/root=.*/& intel_iommu=on iommu=pt/g' /etc/kernel/cmdline
+        elif [ -z "$INTEL_CPU" ] && [ -n "$AMD_CPU" ]
+        then    
+            sed -i 's/root=.*/& amd_iommu=on iommu=pt/g' /etc/kernel/cmdline
+        else
+            echo "Unable to set kernel parameters to enable PCI passthrough, you need to specify if you are using an intel or amd cpu"
+        fi
         
         proxmox-boot-tool refresh
+    
     elif [[ -n "$BIOS" ]]
     then
-        sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet/& intel_iommu=on iommu=pt/g' /etc/default/grub
+        if [ -n "$INTEL_CPU" ] && [ -z "$AMD_CPU" ]
+        then
+            sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet/& intel_iommu=on iommu=pt/g' /etc/default/grub
+        elif [ -z "$INTEL_CPU" ] && [ -n "$AMD_CPU" ]
+        then
+            sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet/& intel_iommu=on iommu=pt/g' /etc/default/grub
+        else
+            echo "Unable to set kernel parameters to enable PCI passthrough, you need to specify if you are using an intel or amd cpu"
+        fi
         
         proxmox-boot-tool refresh
+    
     else
         echo "Unable to set kernel parameters to enable PCI passthrough, you need to specify if you are booting in EFI or BIOS mode"
     fi
+    # Edits the kernel commandline to enable pci passthrough, edits systemdboot configuration for efi boot or grub configuration for bios boot. Requires amd or intel cpu type to be specified.
 
 fi
